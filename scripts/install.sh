cad2456c39ae978ccc963a42332f43e63fbb5716
Kedar <kedar.gn20@gmail.com>
2016-09-22 02:32:56 -0400

    forecast microservice deployment

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,9 +8,9 @@ virtualenv venv >> /var/log/sga-npcomplete-forecast-decision-install.log
 pip install Flask >> /var/log/sga-npcomplete-forecast-decision-install.log
 #echo 'Running Flask Server' >> /var/log/sga-npcomplete-flask-install.log
 export FLASK_APP=forecast_decision.py
-flask run --host=0.0.0.0 --port=65000 >> /var/log/sga-npcomplete-forecast-decision.log 2>&1 &
+flask run --host=0.0.0.0 --port=65000 >> /var/log/sga-npcomplete-forecast-decision-server.log 2>&1 &
 
-cd '/home/ec2-user/ForecastDecision'
+cd '/home/ec2-user/Forecast'
 echo 'Activating virtualenv for Forecast Microservice' >> /var/log/sga-npcomplete-forecast-install.log
 pip install virtualenv >> /var/log/sga-npcomplete-forecast-install.log
 virtualenv venv >> /var/log/sga-npcomplete-forecast-install.log
@@ -18,4 +18,4 @@ virtualenv venv >> /var/log/sga-npcomplete-forecast-install.log
 pip install Flask >> /var/log/sga-npcomplete-forecast-install.log
 #echo 'Running Flask Server' >> /var/log/sga-npcomplete-flask-install.log
 export FLASK_APP=forecast.py
-flask run --host=0.0.0.0 --port=64000 >> /var/log/sga-npcomplete-forecast.log 2>&1 &
+flask run --host=0.0.0.0 --port=64000 >> /var/log/sga-npcomplete-forecast-server.log 2>&1 &
