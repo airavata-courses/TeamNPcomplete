278e495330584728c599f289aaac5ac49882e1a1
Kedar <kedar.gn20@gmail.com>
2016-09-21 23:33:26 -0400

    added log to all cmds of installation

--- ForecastDecision/install.sh
+++ ForecastDecision/install.sh
@@ -1,10 +1,9 @@
-cd '/home/ec2-user/SGA_NPComplete_application/ForecastDecision'
+cd '/home/ec2-user/SGA_NPComplete_application/ForecastDecision' >> /var/log/sga-npcomplete-flask-install.log
 echo 'Activating virtualenv'
-pip install virtualenv
-virtualenv venv
-. venv/bin/activate
-pip install Flask
+pip install virtualenv >> /var/log/sga-npcomplete-flask-install.log
+virtualenv venv >> /var/log/sga-npcomplete-flask-install.log
+. venv/bin/activate >> /var/log/sga-npcomplete-flask-install.log
+pip install Flask >> /var/log/sga-npcomplete-flask-install.log
 echo 'Running Flask Server'
 export FLASK_APP=forecast_decision.py
 flask run --host=0.0.0.0 --port=65000 >> /var/log/sga-npcomplete-flask.log 2>&1 &
-sleep 60
