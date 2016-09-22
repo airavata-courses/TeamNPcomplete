7aa957647bda1d74f6452357d6cd56e86ee1d1f4
Kedar <kedar.gn20@gmail.com>
2016-09-22 00:11:15 -0400

    syntax error fix

--- ForecastDecision/install.sh
+++ ForecastDecision/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-echo 'starting installation process' >> /var/log/sga-npcomplete-flask-install.log
+#echo 'starting installation process' >> /var/log/sga-npcomplete-flask-install.log
 cd '/home/ec2-user/ForecastDecision' >> /var/log/sga-npcomplete-flask-install.log
-echo 'Activating virtualenv' >> /var/log/sga-npcomplete-flask-install.log
+#echo 'Activating virtualenv' >> /var/log/sga-npcomplete-flask-install.log
 pip install virtualenv >> /var/log/sga-npcomplete-flask-install.log
 virtualenv venv >> /var/log/sga-npcomplete-flask-install.log
 . venv/bin/activate >> /var/log/sga-npcomplete-flask-install.log
 pip install Flask >> /var/log/sga-npcomplete-flask-install.log
-echo 'Running Flask Server' >> /var/log/sga-npcomplete-flask-install.log
+#echo 'Running Flask Server' >> /var/log/sga-npcomplete-flask-install.log
 export FLASK_APP=forecast_decision.py
 flask run --host=0.0.0.0 --port=65000 >> /var/log/sga-npcomplete-flask.log 2>&1 &
