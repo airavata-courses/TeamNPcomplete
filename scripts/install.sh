62e9c232b40b7dbd0a4aadff03869320c757ff07
Kedar <kedar.gn20@gmail.com>
2016-09-25 01:54:19 -0400

    made tomcat start sudo

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,4 @@ cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
 mvn -e clean install
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 cd /usr/local/tomcat7/apache-tomcat-7.0.72 >> /var/log/tomcat.log
-./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
+sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
