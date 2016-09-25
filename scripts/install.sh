d86b9af0509b5b6d11019ce6143f987df49deeeb
Kedar <kedar.gn20@gmail.com>
2016-09-25 01:48:39 -0400

    made tomcat background

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,5 @@ echo 'Installing SG_MICROSERVICE_STORMDETECTOR'
 cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
 mvn -e clean install
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
-cd /usr/local/tomcat7/apache-tomcat-7.0.72
-./bin/startup.sh
+cd /usr/local/tomcat7/apache-tomcat-7.0.72 >> /var/log/tomcat.log
+./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
