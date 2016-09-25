c31503811f6ecfe8c5deb7e706460909f5356ee9
Kedar <kedar.gn20@gmail.com>
2016-09-25 03:38:37 -0400

    test commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,5 +7,5 @@ mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 cd '/home/ec2-user/SG_MICROSERVICE_DATAINGESTOR'
 mvn -e clean install
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
-cd /usr/local/tomcat7/apache-tomcat-7.0.72 >> /var/log/tomcat.log
+cd  /usr/local/tomcat7/apache-tomcat-7.0.72 >> /var/log/tomcat.log
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
