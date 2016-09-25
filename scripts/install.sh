c98d53797f96354286148f74219356e07dc2b83e
Kedar <kedar.gn20@gmail.com>
2016-09-25 04:10:30 -0400

    log install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,11 @@
-echo 'Installing SG_MICROSERVICE_STORMDETECTOR'
+echo 'Installing SG_MICROSERVICE_STORMDETECTOR' >> /var/log/tomcat.log
 cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
 mvn -e clean install
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
-
-
+echo 'Installing SG_MICROSERVICE_STORMDETECTOR' >> /var/log/tomcat.log
 cd '/home/ec2-user/SG_MICROSERVICE_DATAINGESTOR'
-mvn -e clean install
+pwd >> /var/log/tomcat.log
+mvn -e clean install >> /var/log/tomcat.log
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
-cd  /usr/local/tomcat7/apache-tomcat-7.0.72 >> /var/log/tomcat.log
+cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
