6bcb202bc9113b8bc08039d35b5e3180340c2433
Kedar <kedar.gn20@gmail.com>
2016-09-25 01:22:14 -0400

    removed redundant code in install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,7 @@
-echo 'Installing Science_Gateway_Apex to Maven'
-cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
-mvn -e clean install
 echo 'Installing SG_MICROSERVICE_STORMDETECTOR'
 cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
 mvn -e clean install
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 cd /usr/local/tomcat7/apache-tomcat-7.0.72
-./bin/startup.sh
-sleep 60
+./bin/startup.sh >> /var/log/tomcat.log
+
