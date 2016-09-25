b1ea43aecd7963f9b1cd975085025f19b8f45895
Kedar <kedar.gn20@gmail.com>
2016-09-25 03:30:35 -0400

    deploy data ingestor

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,10 @@ echo 'Installing SG_MICROSERVICE_STORMDETECTOR'
 cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
 mvn -e clean install
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
+
+
+cd '/home/ec2-user/SG_MICROSERVICE_DATAINGESTOR'
+mvn -e clean install
+mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 cd /usr/local/tomcat7/apache-tomcat-7.0.72 >> /var/log/tomcat.log
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
