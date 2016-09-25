c3890e22c9f7c72ecde1509c36d70c24936bd735
Kedar <kedar.gn20@gmail.com>
2016-09-25 04:45:58 -0400

    added install.sh process to log

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,16 @@
 echo 'Installing SG_MICROSERVICE_STORMDETECTOR' >> /var/log/tomcat.log
 cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
-mvn -e clean install
+mvn -e clean install >> /var/log/tomcat.log
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 echo 'Installing SG_MICROSERVICE_DATAINGESTOR' >> /var/log/tomcat.log
 cd '/home/ec2-user/SG_MICROSERVICE_DATAINGESTOR'
-pwd >> /var/log/tomcat.log
-mvn -e clean install
+mvn -e clean install >> /var/log/tomcat.log
 cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 echo 'Installing SG_MICROSERVICE_STROMCLUSTERING' >> /var/log/tomcat.log
 cd '/home/ec2-user/SG_MICROSERVICE_STROMCLUSTERING'
-mvn -e clean install
+pwd >> /var/log/tomcat.log
+mvn -e clean install >> /var/log/tomcat.log
 cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 
