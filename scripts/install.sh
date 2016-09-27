a1c00c8782819785f1c2ae2f402e62f0f8f209e9
Kedar <kedar.gn20@gmail.com>
2016-09-26 21:16:31 -0400

    made all maven sudo

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,15 @@
 echo 'Installing SG_MICROSERVICE_STORMDETECTOR' >> /var/log/tomcat.log
 cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
-mvn -e clean install >> /var/log/tomcat.log
+sudo mvn -e clean install >> /var/log/tomcat.log
 mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
 echo 'Installing SG_MICROSERVICE_DATAINGESTOR' >> /var/log/tomcat.log
 cd '/home/ec2-user/SG_MICROSERVICE_DATAINGESTOR'
-mvn -e clean install >> /var/log/tomcat.log
+sudo mvn -e clean install >> /var/log/tomcat.log
 cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 echo 'Installing SG_MICROSERVICE_STROMCLUSTERING' >> /var/log/tomcat.log
 cd '/home/ec2-user/SG_MICROSERVICE_STROMCLUSTERING'
-mvn -e clean install >> /var/log/tomcat.log
+sudo mvn -e clean install >> /var/log/tomcat.log
 cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 cd  /usr/local/tomcat7/apache-tomcat-7.0.72
 echo 'Installing SG_MICROSERVICE_REGISTRY' >> /var/log/tomcat.log
